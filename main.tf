03aa2032e42987a7559d502a1149ff7bf9b838aa
Nick Bartosh <nick.bartosh@gmail.com>
2021-10-22 13:05:25 -0500

    aaa

--- main.tf
+++ main.tf
@@ -17,3 +17,23 @@ provider "solidserver" {
   host      = "3.123.22.95"
   sslverify = false
 }
+
+resource "solidserver_ip_space" "space_acmecorp" {
+  name       = "AcmeCorp"
+}
+
+resource "solidserver_ip_space" "CorpOne" {
+  name   = "CorpLifeOne"
+}
+
+resource "solidserver_ip_space" "corp_two" {
+  name   = "CorpLifeTwo"
+}
+
+resource "solidserver_ip_subnet" "azure_internal_1" {
+  space            = "${solidserver_ip_space.corp_two.name}"
+  request_ip       = "10.100.100.0"
+  prefix_size      = 16
+  name             = "Azure Block"
+  terminal         = false
+}
